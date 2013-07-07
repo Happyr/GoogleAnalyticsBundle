4574bb9e404e0c7fa977bae9d32cae250a5bc54e
Nyholm <tobias.nyholm@gmail.com>
2013-07-07 15:48:15 +0200

    added dummy test

---
+++ Tests/Services/TokenServiceTest.php
@@ -0,0 +1,14 @@
+<?php
+
+
+namespace HappyR\Google\AnalyticsBundle\Tests\Services;
+
+
+class TokenServiceTest extends \PHPUnit_Framework_TestCase
+{
+
+    public function testNothing()
+    {
+        $this->assertTrue(true);
+    }
+}

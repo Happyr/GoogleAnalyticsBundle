5388726e19ff9959e3cff4a23752c7fd1ada9d09
Tobias Nyholm <tobias.nyholm@gmail.com>
2014-07-10 09:20:22 +0200

    Added travis and updated tests

--- Tests/bootstrap.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-
-if (file_exists($file = __DIR__ . '/autoload.php')) {
-    include_once $file;
-} elseif (file_exists($file = __DIR__ . '/autoload.php.dist')) {
-    include_once $file;
-}
